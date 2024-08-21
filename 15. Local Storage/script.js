8cb121e1fea7bee7a9fff8dceb32e82605f1a094
sjake12 <apostol.sjake12@gmail.com>
2024-08-21 13:39:55 +0800

    minor change

--- 15. Local Storage/script.js
+++ 15. Local Storage/script.js
@@ -19,13 +19,13 @@ function populateList(plates = [], platesList) {
   platesList.innerHTML = plates
     .map((plate, i) => {
       return `
-            <li>
-                <input type="checkbox" data-index=${i} id="item${i}"  ${
+          <li>
+            <input type="checkbox" data-index=${i} id="item${i}" ${
         plate.done ? "checked" : ""
       }/>
-                <label for="item${i}">${plate.text}</label>
-            </li>
-        `;
+            <label for="item${i}">${plate.text}</label>
+          </li>
+      `;
     })
     .join("");
 }
