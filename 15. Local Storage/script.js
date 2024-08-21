d9960bee179a19214dbfee86be1e4b8b51b89c97
sjake12 <apostol.sjake12@gmail.com>
2024-08-21 20:01:58 +0800

    added delete button

--- 15. Local Storage/script.js
+++ 15. Local Storage/script.js
@@ -9,9 +9,10 @@ function addItem(e) {
     text,
     done: false,
   };
+
   items.push(item);
-  populateList(items, itemsList);
   localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
   this.reset();
 }
 
@@ -19,12 +20,13 @@ function populateList(plates = [], platesList) {
   platesList.innerHTML = plates
     .map((plate, i) => {
       return `
-          <li>
-            <input type="checkbox" data-index=${i} id="item${i}" ${
+        <li>
+          <input type="checkbox" data-index=${i} id="item${i}" ${
         plate.done ? "checked" : ""
       }/>
-            <label for="item${i}">${plate.text}</label>
-          </li>
+          <label for="item${i}">${plate.text}</label>
+          <span class="delete" data-index=${i}>X</span>
+        </li>
       `;
     })
     .join("");
@@ -39,7 +41,17 @@ function toggleDone(e) {
   populateList(items, itemsList);
 }
 
+function toggleDelete(e) {
+  if (!e.target.matches("span")) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items.splice(index, 1);
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
 addItems.addEventListener("submit", addItem);
 itemsList.addEventListener("click", toggleDone);
+itemsList.addEventListener("click", toggleDelete);
 
 populateList(items, itemsList);
