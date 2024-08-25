048047786fcb8d490eab64d5a2148dcf677a1a25
sjake12 <apostol.sjake12@gmail.com>
2024-08-25 12:57:30 +0800

    initial commit

---
+++ 18. Adding up times with Reduce/script.js
@@ -0,0 +1,22 @@
+const timeNodes = Array.from(document.querySelectorAll("[data-time]"));
+
+const seconds = timeNodes
+  .map((node) => node.dataset.time)
+  .map((timeCode) => {
+    const [mins, secs] = timeCode.split(":").map(parseFloat);
+
+    return mins * 60 + secs;
+  })
+  .reduce((total, currentValue) => total + currentValue);
+
+let secondsLeft = seconds;
+
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const minutes = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+const total = [hours, minutes, secondsLeft].join(":");
+
+console.log(total);
